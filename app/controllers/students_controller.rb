8639ef17aadd5d4a093b8b0d27aa196a8edd0515
chris <cseiter@gmail.com>
2021-08-28 16:02:24 -0400

    grades route created

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -4,4 +4,9 @@ class StudentsController < ApplicationController
         students = Student.all
         render json: students
     end
+
+    def grades
+        students = Student.order(grade: :desc)
+        render json: students
+    end
 end
